14226e238bd5a49d1c1f7a56f788316d813aa38d
root <tonmanseven@yandex.ru>
2019-04-12 18:05:26 +0300

    ebala

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from datetime import date
+from blog.views import decript
 # Create your models here.
 
 class Teleofis_state(models.Model):
-    
+
+    title = models.CharField(max_length = 150)
     timestamp = models.DateTimeField(auto_now_add=False)
     status = models.BooleanField(db_index=True)
 
     class Meta:
-        db_table = '"teleofis_state"'
+        db_table = '"{}"'.format(title)
         
     def __str__(self):
         return '{} {}'.format(self.timestamp, self.status)  
