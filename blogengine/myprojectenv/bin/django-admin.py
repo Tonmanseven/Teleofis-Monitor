510b592faf53536c1ceef20536b6b14e5c92b80f
plutonmanseven <plutonmanseven@yandex.ru>
2019-04-12 18:03:18 +0300

    install pip3 and django myenv

---
+++ blogengine/myprojectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/teleofis/Git/teleofismonitor/blogengine/myprojectenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
