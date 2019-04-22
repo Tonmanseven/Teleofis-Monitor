1cb4a75ca652e7da0d56c0513b3968358bb67155
plutonmanseven <plutonmanseven@yandex.ru>
2019-04-22 15:25:22 +0300

    new version models

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -11,75 +11,31 @@ from .forms import UserForm, SensForm
 now = datetime.datetime.now()
 
 
-######## data from luna server ########
- #### IESK ####
-def all_routers_ping():
+######## data from server ########
+def all_routers_ping(hname):
     
+    tele_data = teleping.objects.filter(host = '{}'.format(hname))
+
+    j = 0
+    auff = []
+    host = []
+    inet = []
+    vpn = []
+    dt_pub = []
+    for i in tele_data:
+        auff.append(i)
+        host.append(auff[j].host)
+        inet.append(auff[j].inet)
+        vpn.append(int(auff[j].vpn))
+        dt_pub.append(auff[j].timestamp.strftime('%d-%m-%Y %H:%M'))  
+        j += 1 
 
     opora_all = {
-
-            ########### IESK ############
-        'state_mark_1': state1,
-        'date_mark_1': date_now1,
-
-        'state_mark_2': state2,
-        'date_mark_2': date_now2,
-
-        'state_mark_9': state9,
-        'date_mark_9': date_now9,
         
-        'state_mark_3': state3,
-        'date_mark_3': date_now3,
-
-        'state_mark_4': state4,
-        'date_mark_4': date_now4,
-
-        'state_mark_5': state5,
-        'date_mark_5': date_now5,
-
-        'state_mark_6': state6,
-        'date_mark_6': date_now6,
-
-        'state_mark_7': state7,
-        'date_mark_7': date_now7,
-
-        'state_mark_8': state8,
-        'date_mark_8': date_now8,
-
-        'state_mark_10': state10,
-        'date_mark_10': time_state10,
-
-        'state_mark_11': state11,
-        'date_mark_11': time_state11,
-        
-        'state_mark_12': state12,
-        'date_mark_12': time_state12, 
-
-        'state_mark_13': state13,
-        'date_mark_13': time_state13, 
-
-        'state_mark_14': state14,
-        'date_mark_14': date_now14,
-
-         #### МРСК СЗ ####
-        'state_spb1': state_spb1,
-        'date_spb1': date_spb1,
-
-        'state_spb2': state_spb2,
-        'date_spb2': date_spb2,
-
-        'state_spb3': state_spb3,
-        'date_spb3': date_spb3,
-        #### СИБУР ####
-        'state_spb4': state_spb4,
-        'date_spb4': date_spb4,
-
-        'state_spb5': state_spb5,
-        'date_spb5': date_spb5,
-
-        'state_spb6': state_spb6,
-        'date_spb6': date_spb6,
-           
+        'host_router': host,
+        'state_router': vpn,
+        'date_router': dt_pub,
+            
     }                
 
     # частота выполнений 3600
@@ -94,65 +50,76 @@ def posts_list(request):
 
 def station_1(request):
 
-    data_opora_3 = all_routers_ping()
+    mk1 = all_routers_ping('pluto')
+    mk2 = all_routers_ping('pluto')
+    mk9 = all_routers_ping('luna')
 
-    state1 = data_opora_3['state_mark_1']
-    date1 = data_opora_3['date_mark_1']
-    state2 = data_opora_3['state_mark_2']
-    date2 = data_opora_3['date_mark_2']
-    state9 = data_opora_3['state_mark_9']
-    date9 = data_opora_3['date_mark_9']
+    state1 = mk1['state_router']
+    date1 = mk1['date_router']
+
+    state2 = mk2['state_router']
+    date2 = mk2['date_router']
+
+    state9 = mk9['state_router']
+    date9 = mk9['date_router']
 
     return render(request, 'blog/station_3.html', context={'mark009': state9[0], 'mark002': state2[0],
-                                                           'mark001': state1[0], 'times1': my_datetime1,                                                  
-                                                            'state1': state1, 'times2': my_datetime2,
-                                                            'state2': state2, 'times9': my_datetime9,
+                                                           'mark001': state1[0], 'times1': date1,                                                  
+                                                            'state1': state1, 'times2': date2,
+                                                            'state2': state2, 'times9': date9,
                                                             'state9': state9,
                                                            })
 
 
 def station_2(request):
 
-    data_opora_2 = all_routers_ping()
+    mk3 = all_routers_ping('pluto')
+    mk4 = all_routers_ping('pluto')
+    mk5 = all_routers_ping('luna')
 
-    state3 = data_opora_2['state_mark_3']
-    date3 = data_opora_2['date_mark_3']
-    state4 = data_opora_2['state_mark_4']
-    date4 = data_opora_2['date_mark_4']
-    state5 = data_opora_2['state_mark_5']
-    date5 = data_opora_2['date_mark_5']
+    state3 = mk3['state_router']
+    date3 = mk3['date_router']
 
+    state4 = mk4['state_router']
+    date4 = mk4['date_router']
 
+    state5 = mk5['state_router']
+    date5 = mk5['date_router']
+    
     return render(request, 'blog/station_2.html', context={'mark003': state3[0], 'mark004': state4[0],
-                                                           'mark005': state5[0], 'times3': my_datetime3,
-                                                           'state3': state3, 'times4': my_datetime4,
-                                                           'state4': state4, 'times5': my_datetime5,
+                                                           'mark005': state5[0], 'times3': date3,
+                                                           'state3': state3, 'times4': date4,
+                                                           'state4': state4, 'times5': date5,
                                                            'state5': state5})
 
 
 def station_3(request):
-    data_opora = all_routers_ping()
+   
+    mk6 = all_routers_ping('pluto')
+    mk7 = all_routers_ping('pluto')
+    mk8 = all_routers_ping('luna')
 
-    state6 = data_opora['state_mark_6']
-    date6 = data_opora['date_mark_6']
-    state7 = data_opora['state_mark_7']
-    date7 = data_opora['date_mark_7']
-    state8 = data_opora['state_mark_8']
-    date8 = data_opora['date_mark_8']
+    state6 = mk6['state_router']
+    date6 = mk6['date_router']
 
+    state7 = mk7['state_router']
+    date7 = mk7['date_router']
+
+    state8 = mk8['state_router']
+    date8 = mk8['date_router']
 
     return render(request, 'blog/station_1.html', context={'mark006': state6[0], 'mark007': state7[0],
-                                                           'mark008': state8[0], 'times6': my_datetime6,
-                                                           'state6': state6, 'times7': my_datetime7,
-                                                           'state7': state7, 'times8': my_datetime8,
+                                                           'mark008': state8[0], 'times6': date6,
+                                                           'state6': state6, 'times7': date7,
+                                                           'state7': state7, 'times8': date8,
                                                            'state8': state8})
 
 def test(request):
     useform = UserForm()
-    data_opora = all_routers_ping()
+    data_opora = all_routers_ping('pluto')
 
-    state14 = data_opora['state_mark_14']
-    date14 = data_opora['date_mark_14']
+    state14 = data_opora['state_router']
+    date14 = data_opora['date_router']
 
     if request.method == "POST":
         start = request.POST.get("startDate")
@@ -160,49 +127,54 @@ def test(request):
         print(start)
         
         return render(request, 'blog/test.html', context={ 'form': useform, 'mark014': state14[0],
-                                                           'times14': my_datetime,
+                                                           'times14': date14,
                                                            'state14': state14})
     else:
         return render(request, 'blog/test.html', context={ 'form': useform, 'mark014': state14[0],
-                                                           'times14': my_datetime,
+                                                           'times14': date14,
                                                            'state14': state14})
 
 def fins(request):
     
-    data_fins = all_routers_ping()
+    spb1 = all_routers_ping('pluto')
+    spb2 = all_routers_ping('pluto')
+    spb3 = all_routers_ping('luna')
 
-    state_spb1 = data_fins['state_spb1']
-    date_spb1 = data_fins['date_spb1']
+    state_spb1 = spb1['state_router']
+    date_spb1 = spb1['date_router']
 
-    state_spb2 = data_fins['state_spb2']
-    date_spb2 = data_fins['date_spb2']
+    state_spb2 = spb2['state_router']
+    date_spb2 = spb2['date_router']
 
-    state_spb3 = data_fins['state_spb3']
-    date_spb3 = data_fins['date_spb3']
+    state_spb3 = spb3['state_router']
+    date_spb3 = spb3['date_router']
+
 
     return render(request, 'blog/fins.html', context={     'spb1': state_spb1[0], 'spb2': state_spb2[0], 'spb3': state_spb3[0],
-                                                           'times10': my_sbp1,
-                                                           'state10': state_spb1, 'times11': my_sbp2,
-                                                           'state11': state_spb2, 'times12': my_sbp3,
+                                                           'times10': date_spb1,
+                                                           'state10': state_spb1, 'times11': date_spb2,
+                                                           'state11': state_spb2, 'times12': date_spb3,
                                                            'state12': state_spb3, })
 
 def sibur(request):
 
-    data_fins = all_routers_ping()
+    spb4 = all_routers_ping('pluto')
+    spb5 = all_routers_ping('pluto')
+    spb6 = all_routers_ping('luna')
 
-    state_spb4 = data_fins['state_spb4']
-    date_spb4 = data_fins['date_spb4']
+    state_spb4 = spb4['state_router']
+    date_spb4 = spb4['date_router']
 
-    state_spb5 = data_fins['state_spb5']
-    date_spb5 = data_fins['date_spb5']
+    state_spb5 = spb5['state_router']
+    date_spb5 = spb5['date_router']
 
-    state_spb6 = data_fins['state_spb6']
-    date_spb6 = data_fins['date_spb6']
+    state_spb6 = spb6['state_router']
+    date_spb6 = spb6['date_router']
 
     return render(request, 'blog/sibur.html', context={     'spb4': state_spb4[0], 'spb5': state_spb5[0], 'spb6': state_spb6[0],
-                                                           'times10': my_sbp4,
-                                                           'state10': state_spb4, 'times11': my_sbp5,
-                                                           'state11': state_spb5, 'times12': my_sbp6,
+                                                           'times10': date_spb4,
+                                                           'state10': state_spb4, 'times11': date_spb5,
+                                                           'state11': state_spb5, 'times12': date_spb6,
                                                            'state12': state_spb6, })
 
 def tele_robot(request):
@@ -227,7 +199,14 @@ def tele_robot(request):
         timestamp = buffer["timestamp"] # - время пингования 
         internetStatus = buffer["internetStatus"] # 
         vpnStatus = buffer["vpnStatus"]
-        datetime = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(timestamp))
+        datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
+
+        teleofis_new.host = hostname
+        teleofis_new.timestamp = datetime
+        teleofis_new.inet = internetStatus
+        teleofis_new.vpn = vpnStatus
+
+        teleofis_new.save()
 
         print(hostname, ": ", datetime, ": ", internetStatus, ": ", vpnStatus)
           
--- blogengine/blogengine/settings.py
+++ blogengine/blogengine/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'blogengine.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db_new.sqlite3'),
     }
 }
 
