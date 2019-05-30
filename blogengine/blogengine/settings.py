20c1b7997a14d7e8fad489be87dd31b27cb75d60
plutonmanseven <plutonmanseven@yandex.ru>
2019-05-30 11:46:54 +0300

    post3 with wb

--- blogengine/blogengine/settings.py
+++ blogengine/blogengine/settings.py
@@ -75,16 +75,13 @@ WSGI_APPLICATION = 'blogengine.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    #  'default': {
-    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #    'NAME': 'telemonitor',
-    #    'USER': 'pluto',
-    #    'PASSWORD': 'plutonman',
-    #    'HOST': 'localhost',
-    #    'PORT': '',
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'fins.sqlite3'),
+     'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'telemonitor',
+       'USER': 'pluto',
+       'PASSWORD': 'plutonman',
+       'HOST': 'localhost',
+       'PORT': '',
     }
 }
 
